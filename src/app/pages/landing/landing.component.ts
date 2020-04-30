1878e8b435eada8a264acaf3150c754295e34148
Freddy Cheungh <fgcc1111@gmail.com>
2020-04-30 14:27:09 -0400

    Second page option.

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppLandingComponent {
   title = 'demo';
+
+  constructor(private router: Router) {}
+
+  navigate(): void {
+    this.router.navigateByUrl('about');
+  }
 }
